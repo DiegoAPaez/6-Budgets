90d33f20a80450d5049d595795b49d860b8ef3d7
DiegoAPaez <diego.p31@hotmail.com>
2025-06-23 17:34:21 +0200

    Basic services stored to be accessed by components

---
+++ src/data/services.js
@@ -0,0 +1,17 @@
+export const SERVICES = [
+    {
+        name: 'SEO Optimization',
+        description: 'Improve your website’s visibility on search engines to attract more organic traffic.',
+        price: 300
+    },
+    {
+        name: 'Advertising Campaigns',
+        description: 'Create and manage targeted advertising campaigns to reach your ideal audience.',
+        price: 400
+    },
+    {
+        name: 'Website Development',
+        description: 'Design and develop a professional website tailored to your business needs.',
+        price: 500
+    }
+];
